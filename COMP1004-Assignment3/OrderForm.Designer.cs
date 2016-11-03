6e5ab3c46999602f577c6f2e10917fcf5ec51b0c
200275643 <chandrareddy.mec@gmail.com>
2016-11-03 11:41:06 -0400

    added changes

--- COMP1004-Assignment3/OrderForm.Designer.cs
+++ COMP1004-Assignment3/OrderForm.Designer.cs
@@ -28,11 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MvieSelectedGroupBox = new System.Windows.Forms.GroupBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.CategoryTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.MvieSelectedGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(478, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // MvieSelectedGroupBox
+            // 
+            this.MvieSelectedGroupBox.Controls.Add(this.CategoryTextBox);
+            this.MvieSelectedGroupBox.Controls.Add(this.categoryLabel);
+            this.MvieSelectedGroupBox.Controls.Add(this.TitleTextBox);
+            this.MvieSelectedGroupBox.Controls.Add(this.TitleLabel);
+            this.MvieSelectedGroupBox.Location = new System.Drawing.Point(13, 44);
+            this.MvieSelectedGroupBox.Name = "MvieSelectedGroupBox";
+            this.MvieSelectedGroupBox.Size = new System.Drawing.Size(176, 230);
+            this.MvieSelectedGroupBox.TabIndex = 1;
+            this.MvieSelectedGroupBox.TabStop = false;
+            this.MvieSelectedGroupBox.Text = "Movie Selected";
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(7, 20);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(27, 13);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Title";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(10, 47);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(140, 20);
+            this.TitleTextBox.TabIndex = 1;
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(7, 86);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.categoryLabel.TabIndex = 2;
+            this.categoryLabel.Text = "Category";
+            // 
+            // CategoryTextBox
+            // 
+            this.CategoryTextBox.Location = new System.Drawing.Point(7, 117);
+            this.CategoryTextBox.Name = "CategoryTextBox";
+            this.CategoryTextBox.Size = new System.Drawing.Size(143, 20);
+            this.CategoryTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(208, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 317);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 373);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MvieSelectedGroupBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "OrderForm";
             this.Text = "OrderForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.MvieSelectedGroupBox.ResumeLayout(false);
+            this.MvieSelectedGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.GroupBox MvieSelectedGroupBox;
+        private System.Windows.Forms.TextBox CategoryTextBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- COMP1004-Assignment3/SelectionForm.Designer.cs
+++ COMP1004-Assignment3/SelectionForm.Designer.cs
@@ -31,6 +31,15 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.YourSelectionGroupBox = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.CurrentMoviesLabel = new System.Windows.Forms.Label();
+            this.YourSelectionGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -59,9 +68,15 @@
             // 
             // YourSelectionGroupBox
             // 
+            this.YourSelectionGroupBox.Controls.Add(this.CostLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.CategoryLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.TitleLabel);
+            this.YourSelectionGroupBox.Controls.Add(this.textBox3);
+            this.YourSelectionGroupBox.Controls.Add(this.textBox2);
+            this.YourSelectionGroupBox.Controls.Add(this.TitleTextBox);
             this.YourSelectionGroupBox.Location = new System.Drawing.Point(159, 64);
             this.YourSelectionGroupBox.Name = "YourSelectionGroupBox";
-            this.YourSelectionGroupBox.Size = new System.Drawing.Size(304, 210);
+            this.YourSelectionGroupBox.Size = new System.Drawing.Size(303, 159);
             this.YourSelectionGroupBox.TabIndex = 1;
             this.YourSelectionGroupBox.TabStop = false;
             this.YourSelectionGroupBox.Text = "YourSelection";
@@ -76,16 +91,89 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Choose the movie you wish to Stream from below";
             // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(83, 66);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.ReadOnly = true;
+            this.TitleTextBox.Size = new System.Drawing.Size(192, 20);
+            this.TitleTextBox.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(198, 126);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(83, 47);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(27, 13);
+            this.TitleLabel.TabIndex = 3;
+            this.TitleLabel.Text = "Title";
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Location = new System.Drawing.Point(83, 107);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.CategoryLabel.TabIndex = 4;
+            this.CategoryLabel.Text = "Category";
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(198, 107);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(28, 13);
+            this.CostLabel.TabIndex = 5;
+            this.CostLabel.Text = "Cost";
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(369, 251);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(75, 23);
+            this.NextButton.TabIndex = 3;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // CurrentMoviesLabel
+            // 
+            this.CurrentMoviesLabel.AutoSize = true;
+            this.CurrentMoviesLabel.Location = new System.Drawing.Point(12, 35);
+            this.CurrentMoviesLabel.Name = "CurrentMoviesLabel";
+            this.CurrentMoviesLabel.Size = new System.Drawing.Size(78, 13);
+            this.CurrentMoviesLabel.TabIndex = 4;
+            this.CurrentMoviesLabel.Text = "Current Movies";
+            // 
             // SelectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 286);
+            this.Controls.Add(this.CurrentMoviesLabel);
+            this.Controls.Add(this.NextButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.YourSelectionGroupBox);
             this.Controls.Add(this.listBox1);
             this.Name = "SelectionForm";
             this.Text = "SelectionForm";
+            this.YourSelectionGroupBox.ResumeLayout(false);
+            this.YourSelectionGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,6 +184,14 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox YourSelectionGroupBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Label CurrentMoviesLabel;
     }
 }
 
